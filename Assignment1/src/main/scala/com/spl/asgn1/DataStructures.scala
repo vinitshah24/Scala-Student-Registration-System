4f6ce812d473a9b83ad2e0c01e88f969ddaf490c
Jay Patel <jaydpatel99@gmail.com>
2021-10-15 11:06:32 -0400

    Added Data structure changes

--- Assignment1/src/main/scala/com/spl/asgn1/DataStructures.scala
+++ Assignment1/src/main/scala/com/spl/asgn1/DataStructures.scala
@@ -2,5 +2,36 @@ package com.spl.asgn1
 
 object DataStructures {
   def main(args: Array[String]): Unit = {
+    
+    //Arrays
+    var numbers = Array(8, 5, 3, 4, 7)
+    
+    //Looping through an array
+    for ( i <- numbers ) {
+        println(i)
+    }
+
+    //summing an array of integers
+    var sum = 0;
+    
+    //Loops through and adds all numbers in array
+    for ( i <- 0 to (numbers.length - 1)) {
+        sum += numbers(i);
+    }
+
+    //prints sum
+    println("Sum: " + sum);
+
+    //Lists
+    val students: List[String] = List("Jay", "Vinit", "Rohit", "Prem")
+
+    //checks to see if list is empty
+    println( "Check if students is empty : " + students.isEmpty )
+
+    //prints head of students list
+    println( "Head of students : " + students.head)
+
+    //prints tail of students list
+    println( "Tail of students : " + students.tail)
 
 }

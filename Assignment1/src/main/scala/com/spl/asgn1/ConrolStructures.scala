c44e7f27b11e305e216f0b507b6cc51fc30b8af1
ppate163 <89852987+ppate163@users.noreply.github.com>
2021-10-15 17:25:02 -0400

    Update ConrolStructures.scala

--- Assignment1/src/main/scala/com/spl/asgn1/ConrolStructures.scala
+++ Assignment1/src/main/scala/com/spl/asgn1/ConrolStructures.scala
@@ -3,5 +3,31 @@ package com.spl.asgn1
 object ConrolStructures {
   def main(args: Array[String]): Unit = {
 
+  int x = 10;
+
+  // if else statement
+  if (x < 5) {
+  println("The value of x is less than 5!")
+  }
+  else if (x == 5) {
+  println("The value of x is equal to 5!")
+  }
+  else {
+  println(The value of x is greater than 5!)
+  }
+
+  //for loop
+  for( x <- 1) {
+         println( "Value of x: " + x );
+
+    }
+
+    //while loop
+     while (x < 15) {
+         println( "Value of x: " + x );
+
+    }
+
   }
 }
+
